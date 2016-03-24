b43fe96793a8962c7053464a2da8fd5463306b12
Tristan Marks <tristan.f.marks@gmail.com>
2016-03-23 21:20:36 -0400

    playing around with views and models and such, to have an idea of what to do when I start developing again

--- communitree_app/views.py
+++ communitree_app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.views.generic import View
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+from .forms import FaceForm
+
+
+class Viewy(View):
+    def get(self, request):
+        form = FaceForm()
+        return render(request, "communitree_app/face.html", {'form': form})
+
+    def post(self, request):
+        form = FaceForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect(reverse("communitree_app:facemake"))
+
+
+class Facemake(View):
+    def get(self, request):
+        form = FaceForm(request.POST)
+        # already validated in Viewy.post()?
+        # uh
+        return JsonResponse({'foo':'bar'})
