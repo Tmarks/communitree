6a72b1627112c87ed66e07db5106db880471b2c2
Tristan Marks <tristan.f.marks@gmail.com>
2016-03-24 21:07:57 -0400

    tried to fix my old index.html

--- communitree_app/urls.py
+++ communitree_app/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name="communitree_app"
 urlpatterns = [
+    url(r'^$', views.Index.as_view(), name="index"),
     url(r'^viewy/$', views.Viewy.as_view(), name="viewy"),
-    url(r'^facemake/$', views.Facemake.as_view(), name="facemake")
+    url(r'^facemake/$', views.Facemake.as_view(), name="facemake"),
+    url(r'^querydb/$', views.QueryDB.as_view(), name="querydb"),
 ]
--- communitree_app/views.py
+++ communitree_app/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.views.generic import View
+from django.core import serializers
 from django.core.urlresolvers import reverse
+from .models import CropFeature
 
 from .forms import FaceForm
+
+
+class Index(View):
+    def get(self, request):
+        return render(request, "communitree_app/index.html")
 
 
 class Viewy(View):
@@ -23,3 +30,9 @@ class Facemake(View):
         # already validated in Viewy.post()?
         # uh
         return JsonResponse({'foo':'bar'})
+
+
+class QueryDB(View):
+    def get(self, request):
+        print serializers.serialize("json", CropFeature.objects.all())
+        return JsonResponse({"cropfeatures" : serializers.serialize("json", CropFeature.objects.all())})
