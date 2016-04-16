4be29aa7b8c82e1e1d9ef2d76ca2eb6e2c7cff95
Tristan Marks <tristan.f.marks@gmail.com>
2016-04-15 22:24:01 -0400

    played around until I could send coordinates for a bounding box to Django, use them to query the database for CropFeatures, and display them. Unfortunately, the bounding box didn't seem to contain any of the existing cropfeatures, which contradicts my expectation, but because I took too many Youtube breaks and it's now 10:30, it's not getting solved now.

--- communitree_app/views.py
+++ communitree_app/views.py
@@ -4,6 +4,7 @@ from django.views.generic import View
 from django.core.urlresolvers import reverse
 from .models import CropFeature
 import json
+from django.contrib.gis.geos import Polygon
 
 from .forms import FaceForm
 
@@ -34,6 +35,29 @@ class Facemake(View):
 
 class QueryDB(View):
     def get(self, request):
+        """
+        Send all of the CropFeature polygons from the database within the received Bounding Box.
+        :param request HttpRequest object -- request.GET["bounds"] should be the
+        bounding box as a string.
+        :return JSONResponse containing "cropfeatures", a list of GeoJSON
+        objects representing the CropFeatures found:
+        """
+
+        # request.GET["bounds"] is the return value from LeafletJS's
+        # Map.getBounds().toBBoxString().
+        # That's 'southwest_lng,southwest_lat,northeast_lng,northeast_lat'
+        # So this fits what Polygon.from_bbox expects.
+        # ... except I think if the box crosses the International Date Line, or
+        # if it would containa  pole.
+        # TODO: Might be worth figuring this out some day, though no one's growing
+        # any cucumbers in the middle of the pacific or at the south pole
+        print request.GET
+        bounds = [float(x) for x in request.GET['bounds'].split(',')]
+        bounds_polygon = Polygon.from_bbox(bounds)
+        cropfeatures_in_view = [cf.geojson for cf in CropFeature.objects.filter(mpoly__touches=bounds_polygon)]
+        return JsonResponse({"cropfeatures": cropfeatures_in_view})
+
+        """
         cf = CropFeature.objects.last()
         return JsonResponse({"cropfeature": {"name": cf.name,
                                              "species": cf.species,
@@ -41,12 +65,13 @@ class QueryDB(View):
                                              }
                              })
         # return JsonResponse({"cropfeature" : CropFeature.objects.last().mpoly.geojson})
+        """
 
 
 class JQTesty(View):
     def get(self, request):
         print request.GET
-        print type(request.GET['pancreas'])
+        print type(request.GET)
         resp = {}
         for d in request.GET:
             resp[d.upper()] = request.GET[d]*3
